6ee5456bf7cea0e1ad47a46e7c21bb1c742b86c0
yokinanya <yokinaaa@outlook.com>
2024-07-12 21:04:23 +0800

    移除部分无效后端

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -235,9 +235,6 @@ export default {
           "自动判断客户端": "auto",
         },
         customBackend: {
-          "肥羊增强型后端【vless reality+hy1+hy2】": "https://api.v1.mk",
-          "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
-          "つつ-多地防失联【负载均衡+国内优化】": "https://api.tsutsu.one",
           "nameless13提供": "https://www.nameless13.com",
           "subconverter作者提供": "https://sub.xeton.dev",
           "sub-web作者提供": "https://api.wcc.best",
@@ -245,9 +242,6 @@ export default {
           "星雨沫家里云【纯ipv6】": "https://ddns.yokinanya.icu:5552/subconverter"
         },
         backendOptions: [
-          { value: "https://api.v1.mk" },
-          { value: "https://sub.d1.mk" },
-          { value: "https://api.tsutsu.one" },
           { value: "https://www.nameless13.com" },
           { value: "https://sub.xeton.dev" },
           { value: "https://api.wcc.best" },
